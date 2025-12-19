e27c9af39b2233eaf691cf6eca80582406f9800f
adelso99 <adelsohernandez99@gmail.com>
2025-12-18 23:45:41 -0600

    vamso x987

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-'use strict'
+/*'use strict'
 var numero1 = Number(prompt("Ingrese el Primer Numero"));
 var numero2 = Number(prompt("Ingrese el segundo Numero"));
 
 var suma = numero1+numero2;
 
 alert("la suma es: "+suma);
-console.log(suma);
+console.log(suma);*/
+
+
 
 
 
