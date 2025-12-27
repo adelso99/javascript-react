65c523f357b81ac3e50f963db67428bfa5d1cb47
adelso99 <adelsohernandez99@gmail.com>
2025-12-26 22:18:51 -0600

    vamos x1051

--- main.js
+++ main.js
@@ -16,3 +16,37 @@ Progresss .....1
 Progresss .....2
 */
 
+var lista = [];  // 1 , 2 , 3 , 4
+
+function fizzBuzz(){
+
+    
+    
+        if (lista.length === 0) {
+            lista.push(1);
+            }else {
+                while(nuevoElemento <= 100){
+
+                    var ultimoIndex = lista.length - 1;
+                    var ultimoElemento = ultimoIndex + 1;
+                    var nuevoElemento = ultimoElemento + 1;
+
+                        if (nuevoElemento % 3 === 0 && nuevoElemento % 5 === 0){
+                            lista.push("FizzBuzz");
+                        } else  if (nuevoElemento % 3 === 0){
+                            lista.push("Fizz");
+                        } else if (nuevoElemento % 5 === 0){
+                            lista.push("Buzz");
+                        } else {
+                            lista.push(nuevoElemento);
+                }
+            }
+        }
+    
+
+    console.log(lista);
+  
+}
+
+
+fizzBuzz(); //0
