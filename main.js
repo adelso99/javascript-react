deb102e84e2e7149b4fe5b86f5c5ed1486f53b25
adelso99 <adelsohernandez99@gmail.com>
2025-12-26 20:00:43 -0600

    vamos x1049

--- main.js
+++ main.js
@@ -16,34 +16,3 @@ Progresss .....1
 Progresss .....2
 */
 
-
-var nombres = ["Angela" , "Ben" , "Jenny" , "Michael" , "Chloe"]; 
-
-function whosPaying(nombres) {
-    
-    /******No cambies el código de arriba*******/
-
-    //Escribe tu código aquí.
-     
-       var numeroPersonas = nombres.length;
-       var posicionPersona = Math.floor(Math.random() * numeroPersonas);
-       var personaRandom = nombres[posicionPersona];
-
-       return "¡"+personaRandom+" va a comprar el almuerzo hoy !";
-
-
-
-    /******No cambies el código de abajo*******/
-
-}
-
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-whosPaying(nombres);
-
-
