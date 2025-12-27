96283aadd050f44a3cd79f58fccb65a9f18123ed
adelso99 <adelsohernandez99@gmail.com>
2025-12-26 18:01:05 -0600

    vamos x1047

--- main.js
+++ main.js
@@ -18,15 +18,17 @@ Progresss .....2
 
 
 var nombres = ["Angela" , "Ben" , "Jenny" , "Michael" , "Chloe"]; 
+var inicio = 1;
+var fin = nombres.length;
 
 function whosPaying(nombres) {
     
     /******No cambies el código de arriba*******/
 
     //Escribe tu código aquí.
-    var n = Math.random(); 
-    n = n * nombres.length;  // con el 100 se asigna un rango maximo de 100
-    n = Math.floor(n) + 1;  //con el "+ 1" se asigana un rango minimo de 1
+     
+      // con el 100 se asigna un rango maximo de 100
+
 
     if (n === 1){
         return "¡"+nombres[0]+" va a comprar el almuerzo hoy !";
