c687eb72e6973aae3f5b12e2c2db3c8cf5dceab3
adelso99 <adelsohernandez99@gmail.com>
2025-12-27 19:59:13 -0600

    vamos z1054

--- main.js
+++ main.js
@@ -17,30 +17,43 @@ Progresss .....2
 */
 
 var cerveza = [];
-var contar = 1;
+var botellas = 6;
 
 function cervezas(){
-    while (contar <= 6){
-        var dato = cerveza.length; 
+    while (botellas <= 1){
+        var total = cerveza.length;
+        var contar = (botellas - 1); 
 
-        if (dato === 5) {
-            cerveza.push("No más botellas de cerveza en la pared, no más botellas de cerveza. Ve a la tienda y compra más, "+dato+" botellas de cerveza en la pared.");
-        } else if (dato === 4) {
-            cerveza.push(contar+" botellas de cerveza en la pared, "+dato+" botellas de cerveza. Toma una y pásala, no más botellas de cerveza en la pared.");
+        if (botellas === 1) {
+            cerveza.push(botellas+" botellas de cerveza en la pared, "+botellas+" botellas de cerveza. Toma una y pásala, no más botellas de cerveza en la pared.");
+        } else if (botellas === 0) {
+            cerveza.push("No más botellas de cerveza en la pared, no más botellas de cerveza. Ve a la tienda y compra más, "+total+" botellas de cerveza en la pared.");
         } else {
-            cerveza.push(contar+" botellas de cerveza en la pared, "+contar+" botellas de cerveza. Toma una y pásala, "+contar+" botellas de cerveza en la pared.");
+            cerveza.push(botellas+" botellas de cerveza en la pared, "+botellas+" botellas de cerveza. Toma una y pásala, "+contar+" botellas de cerveza en la pared.");
         }
 
-        contar++;
+        botellas--;
+
     }    
 
 	console.log(cerveza);
 
 }
 
+cervezas();
+
 /*
 99 botellas de cerveza en la pared, 99 botellas de cerveza.
 Toma una y pásala, 98 botellas de cerveza en la pared
+
+4 botellas de cerveza en la pared, 4 botellas de cerveza.
+Toma una y pásala, 3 botellas de cerveza en la pared.
+
+3 botellas de cerveza en la pared, 3 botellas de cerveza.
+Toma una y pásala, 2 botellas de cerveza en la pared.
+
+2 botellas de cerveza en la pared. 2 botellas de cerveza.
+Toma una y pásala. 1 botella de cerveza en la pared.
 
 1 botella de cerveza en la pared, 1 botella de cerveza.
 Toma una y pásala, no más botellas de cerveza en la pared.
