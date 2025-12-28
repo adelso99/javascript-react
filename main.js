3783d68b8e4abc7e11eeada74b66e786975c96ce
adelso99 <adelsohernandez99@gmail.com>
2025-12-27 23:24:47 -0600

    vamos x1060

--- main.js
+++ main.js
@@ -40,6 +40,26 @@ cervezas();
 
 
 
+var lista1 = [];
 
+function fizzBuzz(){
+    for (var contar = 1; contar <= 100; contar++){
+        if (contar % 3 === 0 && contar % 5 === 0) {
+            lista1.push("FizzBuzz");
+        } else if (contar % 3 === 0) {
+            lista1.push("Fizz");
+        } else if (contar % 5 === 0) {
+            lista1.push("Buzz");
+        } else {
+            lista1.push(contar);
+        }
+
+    }    
+
+	console.log(lista1);
+
+}
+
+fizzBuzz(); // cuando se llama muestra 100 datos
 
 
