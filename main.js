0506dac07a348d55258e633ad8ded98ea9d66788
adelso99 <adelsohernandez99@gmail.com>
2025-12-28 23:05:44 -0600

    1067

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/*'use strict'
-var numero1 = Number(prompt("Ingrese el Primer Numero"));
-var numero2 = Number(prompt("Ingrese el segundo Numero"));
-
-var suma = numero1+numero2;
-
-alert("la suma es: "+suma);
-console.log(suma);*/
-
-
-
-
-
-
-
 
 
 
