b6df87c0214eb307000b80a8bdfe9ec74cd88b9c
adelso99 <adelsohernandez99@gmail.com>
2025-12-28 22:08:03 -0600

    1063

--- main.js
+++ main.js
@@ -11,32 +11,10 @@ console.log(suma);*/
 
 
 
-var lista = []; var numero1 = 0; var numero2 = 1;
 
-function fibonacciGenerator (n) {
-    for (var fibo = 1; fibo <= n; fibo++){
-        
-        
-         if (fibo === 1) {
-            lista.push(numero1);
-        } else if (fibo === 2) {
-            lista.push(numero2);
-        } else if (fibo === 3){
-            lista.push(numero1+numero2);
-        } else {
-            numero1 = numero2;
-            numero2 = numero1 + numero1;
 
-            lista.push(numero2);
-        }
 
-    }    
 
-	console.log(lista);
-
-}
-
-fibonacciGenerator(5); 
 
 
 
