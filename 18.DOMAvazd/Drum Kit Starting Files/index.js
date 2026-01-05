0602c2fae4e6cb95e7426089efbc706d1c69d24e
adelso99 <adelsohernandez99@gmail.com>
2026-01-04 22:20:57 -0600

    vamos x1116

--- 18.DOMAvazd/Drum Kit Starting Files/index.js
+++ 18.DOMAvazd/Drum Kit Starting Files/index.js
@@ -1,10 +1,28 @@
 
+
+
+var datoBotones = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < datoBotones; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", clickBoton);
+
+    function clickBoton(){
+        alert("Has realizado un click: ");
+    }
+
+}
+
+
+/*
+Solucion 1:
 document.querySelector(".set").addEventListener("click", clickBoton);
 
 function clickBoton(){
     alert("Has realizado un click: ");
 }
 
+*/
 
 
 /*
