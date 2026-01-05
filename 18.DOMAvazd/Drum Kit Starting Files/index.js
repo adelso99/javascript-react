d26e663ab163f7a1e2e8e488ba5c0909c866280c
adelso99 <adelsohernandez99@gmail.com>
2026-01-04 22:17:00 -0600

    vamos x1115

--- 18.DOMAvazd/Drum Kit Starting Files/index.js
+++ 18.DOMAvazd/Drum Kit Starting Files/index.js
@@ -1,7 +1,17 @@
 
-document.querySelector("button").addEventListener("click", clickBoton);
-
+document.querySelector(".set").addEventListener("click", clickBoton);
 
 function clickBoton(){
     alert("Has realizado un click: ");
 }
+
+
+
+/*
+FUNCION ANONIMA
+document.querySelector("button").addEventListener("click", function (){
+    alert("Has realizado un click: ");
+});
+
+    
+*/
