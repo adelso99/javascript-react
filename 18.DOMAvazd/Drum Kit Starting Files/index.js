cdf5a039b8b83503105ffd828613be9cca7ad81e
adelso99 <adelsohernandez99@gmail.com>
2026-01-05 20:15:31 -0600

    vamos x1120

--- 18.DOMAvazd/Drum Kit Starting Files/index.js
+++ 18.DOMAvazd/Drum Kit Starting Files/index.js
@@ -1,18 +1,77 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 var datoBotones = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < datoBotones; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", clickBoton);
 
-    function clickBoton(){
-        alert("Has realizado un click: ");
-    }
-
 }
 
+function clickBoton(){
+    alert("Has realizado un click: ");
+}
+
+esto sucede internamente cuando se ejecuta el for y espera hasta que se haga click
+document.querySelectorAll(".drum")[0].addEventListener("click", clickBoton); <-- b1
+document.querySelectorAll(".drum")[1].addEventListener("click", clickBoton); <-- b2
+document.querySelectorAll(".drum")[2].addEventListener("click", clickBoton); <-- b3
+document.querySelectorAll(".drum")[3].addEventListener("click", clickBoton); <-- b4
+document.querySelectorAll(".drum")[4].addEventListener("click", clickBoton); <-- b5
+document.querySelectorAll(".drum")[5].addEventListener("click", clickBoton); <-- b6
+document.querySelectorAll(".drum")[6].addEventListener("click", clickBoton); <-- b7
+*/
 
 
 /*
@@ -21,6 +80,9 @@ document.querySelector(".set").addEventListener("click", clickBoton);
 function clickBoton(){
     alert("Has realizado un click: ");
 }
+
+
+
 */
 
 
--- main.js
+++ main.js
@@ -2,15 +2,26 @@
 var lista = [];
 var numero = 1;
 
-function repaso(){
-    lista.push(numero)
+function repaso(dato){
 
-    numero++;
+    for (var numero = 1; numero <= dato; numero++){
+        if (numero % 3 === 0 && numero % 5 === 0){
+            lista.push("FizzBuzz");
+        }else if (numero % 3 === 0){
+            lista.push("Fizz");
+        }else if (numero % 5 === 0){
+            lista.push("Buzz");
+        } else {
+            lista.push(numero)
+        }
+
+        
+
+    } 
 
     console.log(lista);
 
 }
 
-repaso();
-repaso();
-repaso();
+
+repaso(15);
