32a8d0a9a4395d57cbd1ed57487645d4637142da
adelso99 <adelsohernandez99@gmail.com>
2026-01-05 22:40:14 -0600

    vamos x1123

--- 18.DOMAvazd/Drum_Kit_Starting_Files/main.js
+++ 18.DOMAvazd/Drum_Kit_Starting_Files/main.js
@@ -2,105 +2,11 @@
 var dato = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < dato; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", clickBoton); //aca se va estar cargando cada boton del index del var dato
-}
-
-
-function clickBoton(){
-    alert("Has hecho un Click");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-var datoBotones = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i < datoBotones; i++){
-
-    document.querySelectorAll(".drum")[i].addEventListener("click", clickBoton);
-
-}
-
-function clickBoton(){
-    alert("Has realizado un click: ");
-}
-
-esto sucede internamente cuando se ejecuta el for y espera hasta que se haga click
-document.querySelectorAll(".drum")[0].addEventListener("click", clickBoton); <-- b1
-document.querySelectorAll(".drum")[1].addEventListener("click", clickBoton); <-- b2
-document.querySelectorAll(".drum")[2].addEventListener("click", clickBoton); <-- b3
-document.querySelectorAll(".drum")[3].addEventListener("click", clickBoton); <-- b4
-document.querySelectorAll(".drum")[4].addEventListener("click", clickBoton); <-- b5
-document.querySelectorAll(".drum")[5].addEventListener("click", clickBoton); <-- b6
-document.querySelectorAll(".drum")[6].addEventListener("click", clickBoton); <-- b7
-*/
-
-
-/*
-document.querySelector(".set").addEventListener("click", clickBoton);
-
-function clickBoton(){
-    alert("Has realizado un click: ");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("Has hecho un Click");
+    }); //aca se va estar cargando cada boton del index del var dato
 }
 
 
 
-*/
 
-
-
-/*
-FUNCION ANONIMA
-document.querySelector("button").addEventListener("click", function (){
-    alert("Has realizado un click: ");
-});
-
-    
-*/
