ea54f77eb3da9a476aa8465ea4c5f019627cc932
adelso99 <adelsohernandez99@gmail.com>
2026-01-06 22:27:52 -0600

    vamos x1131

--- 18.DOMAvazd/Drum_Kit_Starting_Files/main.js
+++ 18.DOMAvazd/Drum_Kit_Starting_Files/main.js
@@ -5,11 +5,17 @@ var dato = document.querySelectorAll(".drum").length;
 for(var i = 0; i < dato; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-        var audio = new Audio("./sounds/tom-1.mp3");
-        
-        audio.play();
+        console.log(this.style.color = "white");
+
     }); 
 }
 
 
 
+
+/*
+    var audio = new Audio("./sounds/tom-1.mp3");
+    
+    audio.play();
+
+*/
