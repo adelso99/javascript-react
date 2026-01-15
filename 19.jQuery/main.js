39b582ffaac9c98efe260e0bbb79af8c7d60485f
adelso99 <adelsohernandez99@gmail.com>
2026-01-15 13:14:12 -0600

    vamos x1191

--- 19.jQuery/main.js
+++ 19.jQuery/main.js
@@ -1,1 +1,10 @@
 
+//para cambiar color con css usando jQuery
+//$("h1").css("color", "green");
+
+//para obtener las propiedades usadas del color de css usando jQuery
+console.log($("h1").css("color"));
+
+//para obtener las propiedades usadas font-size con jQuery
+console.log($("h1").css("font-size"));
+
