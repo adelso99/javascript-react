5a238fb2e2e9dd13369688e671f62ba3264f0372
adelso99 <adelsohernandez99@gmail.com>
2026-01-14 19:48:17 -0600

    vamos x1187

--- 19.jQuery/main.js
+++ 19.jQuery/main.js
@@ -1,6 +1,14 @@
 
 
+//Forma 1: Normal el script de jQuery al final del body antes que este archivo js
+$("h1").css("color", "red");
 
+
+/*
+Forma 2: el script de jQuery en el head, donde tambien esta el link de css:
+$(document).ready(function() {
+    $("h1").css("color", "red");
+});*/
 
 
 
