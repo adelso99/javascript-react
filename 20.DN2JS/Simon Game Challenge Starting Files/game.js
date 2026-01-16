018e723585df998d0eb449a67585af1748192453
adelso99 <adelsohernandez99@gmail.com>
2026-01-16 17:57:20 -0600

    vamos x120

--- 20.DN2JS/Simon Game Challenge Starting Files/game.js
+++ 20.DN2JS/Simon Game Challenge Starting Files/game.js
@@ -10,7 +10,33 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     //selecionar el boton que se esta generando
-    $("#"+randomChosenColour).animate({opacity: 0.5}).animate({opacity: 1});
+    let color = $("#"+randomChosenColour)
+    
+    //Animacion de boton
+    color.animate({opacity: 0.2}).animate({opacity: 1});
+
+    switch (randomChosenColour) {
+        case "blue": let blue = new Audio("./sounds/blue.mp3");
+        blue.play();
+            break;
+
+        case "green": let green = new Audio("./sounds/green.mp3");
+        green.play();
+            break;
+
+        case "red": let red = new Audio("./sounds/red.mp3");
+        red.play();
+            break;
+
+        case "yellow": let yellow = new Audio("./sounds/yellow.mp3");
+        yellow.play();
+            break;
+    
+        default:
+            let wrong = new Audio("./sounds/wrong.mp3");
+            wrong.play();
+            break;
+    }
     
 
     return randomChosenColour;
