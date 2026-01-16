7c63627d93358dfe62cd24566475bc28a1dab45c
adelso99 <adelsohernandez99@gmail.com>
2026-01-15 18:07:29 -0600

    vamos x1197

--- 19.jQuery/main.js
+++ 19.jQuery/main.js
@@ -1,3 +1,6 @@
 
-//para establacer el valor del atrinuto se hace asi:
-$("a").attr("src", "https://www.crunchyroll.com/"); //con el segundo parametro Ingresado cambia el valor que se tiene en esa etiqueta
+
+//estamos etableciendo un detector de evento de pulsaciones o teclado de elementos del HTML con jQuery
+$("input").keydown(function(event) {  // estamos regitrado cada vez que se preciona el teclado dentro del "input"
+    console.log(event.key);  // la funcion anonima nos pasara el event que sera el evento o la tecla que esta siendo presionada
+});                          // el ".key" es la palabra recebervada del evento para que solo nos devuelva la letra del teclado presionado
