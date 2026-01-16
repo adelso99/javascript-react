435a079bda87197de7165b9646582ebba6c7b625
adelso99 <adelsohernandez99@gmail.com>
2026-01-15 18:13:03 -0600

    vamos x11998

--- 19.jQuery/main.js
+++ 19.jQuery/main.js
@@ -1,6 +1,6 @@
 
 
-//estamos etableciendo un detector de evento de pulsaciones o teclado de elementos del HTML con jQuery
-$("input").keydown(function(event) {  // estamos regitrado cada vez que se preciona el teclado dentro del "input"
+//estamos etableciendo un detector de evento de pulsaciones o teclado a todo el HTML con jQuery
+$("body").keydown(function(event) {  // estamos regitrado cada vez que se preciona el teclado puede ser "body" o "document"
     console.log(event.key);  // la funcion anonima nos pasara el event que sera el evento o la tecla que esta siendo presionada
 });                          // el ".key" es la palabra recebervada del evento para que solo nos devuelva la letra del teclado presionado
