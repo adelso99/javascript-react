894b6ce6f4474fdfb468f7712c8e205b3838b7cc
adelso99 <adelsohernandez99@gmail.com>
2026-01-15 22:50:13 -0600

    vamos x1201

--- 19.jQuery/main.js
+++ 19.jQuery/main.js
@@ -1,10 +1,4 @@
+$("button").on("click", function(){
+    $("h1").fadeOut();  // este es mas progresivo en la desparcion del h1
+});
 
-//".prepend()" 
-$("h1").prepend("<button>Prepend</button>") //selecionamos el h1 para que ese boton se agregue despues de la etiqueta de apertura del h1
-                                          // osea esta dentro de la etiqueta del h1, esta justo antes del texto del h1
-
-
-
-//".append()"
-$("h1").append("<button>Append</button>") //selecionamos el h1 para que ese boton se agregue despues de la etiqueta de apertura del h1
-                                         // osea esta dentro de la etiqueta del h1, pero justo al finalizar del texto del h1
