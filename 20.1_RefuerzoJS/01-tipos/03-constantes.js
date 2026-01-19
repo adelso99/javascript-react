cb329eff7ca273c657bb657de19518307c0e85a4
adelso99 <adelsohernandez99@gmail.com>
2026-01-18 22:35:33 -0600

    vamos x1231

--- 20.1_RefuerzoJS/01-tipos/03-constantes.js
+++ 20.1_RefuerzoJS/01-tipos/03-constantes.js
@@ -1,6 +1,1 @@
 
-const nombre = "Hola de Nuevo"; //las variables let no se puede reasignarse de nuevo
-nombre = "Hello :D"           //por lo que no se podra cambiar el valor de esa varible
-
-console.log(nombre);
-
