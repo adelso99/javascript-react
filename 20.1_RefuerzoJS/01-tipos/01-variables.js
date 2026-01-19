1d485cd38ae119bd65d6579463de55edbcbe8708
adelso99 <adelsohernandez99@gmail.com>
2026-01-18 20:03:50 -0600

    vamos x1226

--- 20.1_RefuerzoJS/01-tipos/01-variables.js
+++ 20.1_RefuerzoJS/01-tipos/01-variables.js
@@ -1,3 +1,13 @@
-let nombre = "Adelso";
 
+let nombre = "Adelso";
 console.log(nombre);
+
+
+
+let nombreCompleto; //uso de camelCase
+console.log(nombreCompleto);
+
+
+
+let apellido;    //Caja vacia
+apellido = "Hola";  // ya se le asigna un valor a la caja
