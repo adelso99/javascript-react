7b7bcdd1c9267cf1376e936c7517b2f043b6d57f
adelso99 <adelsohernandez99@gmail.com>
2026-01-18 22:26:39 -0600

    vamos x1229

---
+++ 20.1_RefuerzoJS/01-tipos/02-primitivos.js
@@ -0,0 +1,17 @@
+
+//Tipos de datos Primitivos
+
+let numero = 1; // 1.Tipo Number es de Numero
+
+let texto = "Hola"; // 2.Tipo String es de texto
+
+let verdadero = true; // 3.Tipo Boolean es verdadero o falso
+
+let falso = false; // 3.1 Tipo Boolean es falso o verdadero
+
+let noDefinido; // 4.dice any pero es undefined osea no tien un valor
+
+let undef = undefined; // 4.1 undefined pero es undefined osea no tiene un valor
+
+let nulo = null; // 5. tiene un valor pero el valor es nulo no undefined
+
