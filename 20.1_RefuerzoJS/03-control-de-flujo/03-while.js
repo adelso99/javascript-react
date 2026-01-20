9abcc13d120a5a01bbb23b0b027b3dbd1b43db02
adelso99 <adelsohernandez99@gmail.com>
2026-01-20 12:48:28 -0600

    vamos x1245

--- 20.1_RefuerzoJS/03-control-de-flujo/03-while.js
+++ 20.1_RefuerzoJS/03-control-de-flujo/03-while.js
@@ -1,2 +1,21 @@
 //Condicion While
 
+/*
+while (condicion) {
+
+}
+*/
+
+//cuales son los numeros pares
+let i = 0;
+
+while (i <= 10) {
+
+    if (i % 2 === 0) {
+        console.log("Numeros Pares: "+i); //muestra en consola
+    }
+
+    i++; //incremento de la variable i
+}
+
+console.log("Fuera del While");
