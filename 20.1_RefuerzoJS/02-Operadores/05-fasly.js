1a999295d34a03015f6188303778a3b760784280
adelso99 <adelsohernandez99@gmail.com>
2026-01-19 22:48:09 -0600

    vamos x1243

--- 20.1_RefuerzoJS/02-Operadores/05-fasly.js
+++ 20.1_RefuerzoJS/02-Operadores/05-fasly.js
@@ -1,1 +1,30 @@
-//Use de Operadores vistos
+//Use de Operadores vistos --> short-ciruit
+
+//Falso
+/* 
+- seria el Boolean de false
+- 0
+- '' , " "
+- null
+- undefined
+- NaN
+
+Todos estos se conocen como "Falsy"
+*/
+
+//let nombre = ""; // si se muestra en consola muestra Anonimo porque nombre esta vacio es false
+let nombre = "John"; // si se muestra en consola muestra John porque nombre es true no esta vacio
+let username = nombre || "Anonimo";
+console.log(username);
+
+function fn1(){
+    console.log("Soy Funcion 1");
+    return false;
+}
+
+function fn2(){
+    console.log("Soy Funcion 2");
+    return true;
+}
+
+let funciones = fn1() && fn2(); // solo devolvera la primera funcion ya que ambas no returnan true o no sonm iguales
