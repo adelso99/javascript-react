e03762be9cd1be8de84e0432d7c8715b651dbf92
adelso99 <adelsohernandez99@gmail.com>
2026-01-20 16:58:10 -0600

    vamos x1246

---
+++ 20.1_RefuerzoJS/03-control-de-flujo/04-do-while.js
@@ -0,0 +1,12 @@
+//Condicion Do While-- Evalua la condicion al final
+// do {} while ();
+//cuales son los numeros pares
+let i = 2;
+
+do {
+    if (i % 2 === 0) {
+        console.log("Numeros Pares: "+i); //muestra en consola
+    }
+    i++; //incremento de la variable i
+
+} while (i <2);
