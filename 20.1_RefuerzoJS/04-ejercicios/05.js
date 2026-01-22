1added1d62169a45da8373d5dce48cfd87f7c368
adelso99 <adelsohernandez99@gmail.com>
2026-01-21 22:45:58 -0600

    vamos 1257

--- 20.1_RefuerzoJS/04-ejercicios/05.js
+++ 20.1_RefuerzoJS/04-ejercicios/05.js
@@ -6,6 +6,21 @@
 let array = [2, 5 , 7, 15, -5, -100, 55];
 
 function getMenorMayor(arr){
+    let menor = arr[0];
+    let mayor = arr[0];
+
+    for (let i = 0; i < arr.length; i++){
+        if(menor > arr[i] ){
+            menor = arr[i];
+        }
+        
+        if(mayor < arr[i]){
+            mayor = arr[i];
+        }
+    }
+
+    return [menor, mayor];
+    
 
 }
 
