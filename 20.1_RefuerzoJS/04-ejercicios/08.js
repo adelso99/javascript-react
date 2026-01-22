bd9e0f04b034143db31b88475d3da924aa28a8ca
adelso99 <adelsohernandez99@gmail.com>
2026-01-21 23:56:23 -0600

    vamos 1260

--- 20.1_RefuerzoJS/04-ejercicios/08.js
+++ 20.1_RefuerzoJS/04-ejercicios/08.js
@@ -1,3 +1,24 @@
 
 
 //Ejercicio 8
+/**
+ * Crear un algoritmo que tome un arrat de
+ * objeto y devuela un array de pares
+ */
+
+let array = [{
+    id: 1,
+    name: "Nicolas",
+}, {
+    id: 2,
+    name: "Felipe", 
+}, {
+    id: 3,
+    name: "John",
+}];
+
+let pares = [
+    [1, { id: 1, name: "Nicolas"},
+    [1, { id: 2, name: "Felipe"},
+    [1, { id: 3, name: "John"}],
+];
