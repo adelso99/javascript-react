d07e328a4a35c9ece3d27f750e349e51488201ef
adelso99 <adelsohernandez99@gmail.com>
2026-01-21 23:51:52 -0600

    vamos 1259

--- 20.1_RefuerzoJS/04-ejercicios/07.js
+++ 20.1_RefuerzoJS/04-ejercicios/07.js
@@ -2,14 +2,25 @@
 //Ejercicio 7
 /**
  * Crear algoritmo que devuelva 
- * el precio del producto n
+ * el precio del producto 
  * mas impuesto
  */
 
 function precioCompleto(precio, impuesto){
+
+    let impuestoFinal = precio * impuesto;
+    let precioFinal = precio + impuestoFinal;
+
+    return ["este es el Impuesto del Producto: "+impuestoFinal, "este es el Precio del Producto: "+ precioFinal];
+
+    /** Forma 2
+      return precio + (precio * impuesto);
+     
+     */
 
 }
 
 let resultado = precioCompleto(19.90, 0.15);
 
 console.log(resultado);
+
