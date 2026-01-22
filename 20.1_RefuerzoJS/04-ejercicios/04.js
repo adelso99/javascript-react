504e337d3ab6836f1c2f9bfcd1cc4d273a05e15d
adelso99 <adelsohernandez99@gmail.com>
2026-01-21 19:20:30 -0600

    vamos 1255

---
+++ 20.1_RefuerzoJS/04-ejercicios/04.js
@@ -0,0 +1,25 @@
+
+
+//Ejericio 4
+//Crean un algoritmo que imprima numeros impares
+
+let numero = 10;
+
+for (let i = 1; i <= numero; i+=2){
+    
+            console.log("El numero Impar es: " + i);
+    
+
+}
+
+/* opcion 2
+let i = 0;
+while (i <= 10){
+    if( i % 2 !== 0){
+        console.log("Numero Impar: " + i);
+    }
+
+    i++;
+}
+*/
+
