d56f37275bea03fa022ffb4614ce549de38848d3
adelso99 <adelsohernandez99@gmail.com>
2026-01-21 19:23:10 -0600

    vamos 1256

---
+++ 20.1_RefuerzoJS/04-ejercicios/05.js
@@ -0,0 +1,13 @@
+
+//Ejericio 5
+//Crean un algoritmo que devuelva numero
+//menor y mayor de un array
+
+let array = [2, 5 , 7, 15, -5, -100, 55];
+
+function getMenorMayor(arr){
+
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
