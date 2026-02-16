efecb3226e08de932314fa0cc5ef9bd9ac6d8a47
adelso99 <adelsohernandez99@gmail.com>
2026-02-15 23:42:38 -0600

    vamos x1462

--- 20.2_RefuerzoJS_2/JavaScript/leccion-10/10c-archivos/main.js
+++ 20.2_RefuerzoJS_2/JavaScript/leccion-10/10c-archivos/main.js
@@ -2,15 +2,23 @@
 function juego(numero){
   const dato = document.querySelectorAll(".programar")[numero];
 
-
-  if (!dato.classList.contains('solicitar1')) {
+  if(!document.querySelector(".solicitar1")){
+      if (!dato.classList.contains('solicitar1')) {
         dato.classList.add('solicitar1');
       } else {
         dato.classList.remove('solicitar1');
       }
+  }else {
+    dato.classList.remove('solicitar1');
+  }
 
 
 }
+
+/*
+10g. Modifica 10f para que solo 1 botón pueda estar ENCENDIDO a la vez.
+    Juegos  Música   Tecnología
+*/
 
 
 
