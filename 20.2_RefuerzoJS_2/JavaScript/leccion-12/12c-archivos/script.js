16b7954b0248a799a73fc20a320ef3369e938fa4
adelso99 <adelsohernandez99@gmail.com>
2026-02-24 11:59:33 -0600

    vamos x1536

---
+++ 20.2_RefuerzoJS_2/JavaScript/leccion-12/12c-archivos/script.js
@@ -0,0 +1,78 @@
+let lista = [{
+      name: 'Programar', 
+      fecha: "22-03-2022"
+      },
+
+      {
+      name: 'Ir al Gym',
+      fecha: "23-03-2022"
+      }];
+
+
+const datosGuardados = localStorage.getItem("tareas");
+
+if(datosGuardados !== null){
+      lista = JSON.parse(datosGuardados);
+}
+
+
+
+añadirHtml();// llamado de funcion para ver elementos en la web
+
+
+function añadirHtml(){
+      let listaHtml = "";
+
+      lista.forEach(function(elementoObject, index){
+            const {name, fecha} = elementoObject;
+  
+            const datoHtml = `
+            <div>${name}</div>
+            <div>${fecha}</div> 
+              <button onclick="
+                    lista.splice(${index}, 1);
+                    localStorage.setItem('tareas', JSON.stringify(lista));
+                    añadirHtml();  
+              " class="btn-eliminar" >Eliminar</button>
+            `;
+  
+            listaHtml += datoHtml;
+            
+      });
+
+      document.querySelector(".js-elementos-lista").innerHTML = listaHtml;
+
+
+}
+
+
+
+function añadirLista(){
+
+      const dato = document.querySelector(".js-dato");
+      const name = dato.value;
+
+      const fechaInput = document.querySelector(".js-fecha");
+      const fecha = fechaInput.value;
+
+      lista.push({
+            /*Los Objeto tambien tiene atajos hacer esto:
+            name: name,
+            fecha: fecha, 
+            es lo mismo que lo de abajo --> propiedad abreviada
+            */
+            name,
+            fecha,
+      });
+
+      dato.value = "";
+
+
+      añadirHtml();// llamado de funcion para ver actualice los elementos en la web
+
+      localStorage.setItem("tareas", JSON.stringify(lista));
+
+
+}
+
+
