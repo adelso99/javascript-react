e8cacc0ff47aca7acdb9d65b9be81dd62ed85f82
adelso99 <adelsohernandez99@gmail.com>
2026-03-02 23:02:47 -0600

    vamos x1584

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
@@ -42,14 +42,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-45.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            ${(product.priceCents * 100) / 100}
+            ${product.priceCents  / 100}
           </div>
 
           <div class="product-quantity-container">
