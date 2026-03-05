f23b9e315c860af5888dbb25b11b46d3db5f2864
adelso99 <adelsohernandez99@gmail.com>
2026-03-05 17:22:23 -0600

    vamos x1604

---
+++ 20.2_RefuerzoJS_2/JavaScript/leccion-13 - javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,102 @@
+
+let productsHTML = "";
+
+products.forEach((product) => {
+
+  //Patron Acumulador
+  productsHTML += `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents  / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select clase="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+  `;
+
+});
+
+//agregando con ayuda de DOM el HTML generado a la web
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+
+//añadiendo evento al boton de agregar para hacerlo interactivo
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () =>{ 
+      const productId = button.dataset.productId;
+
+      //Parte de la Logica de la cantidad de Productos que se desean agregar al carrito
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+      });
+
+      const quantitySelector = document.querySelector(".js-quantity-selector-${product.id}");
+      const quantity = Number(quantitySelector.value);
+ 
+      if(matchingItem){
+        matchingItem.quantity += quantity;
+      }else{
+        cart.push({
+         productId: productId,
+         quantity: quantity
+        });
+      }
+
+      //Parte de la Logica del Carrito de Productos
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+
+  });
+});
