c22fc00bf95c7a7c801392b60331cf98249a4d44
adelso99 <adelsohernandez99@gmail.com>
2026-03-04 23:25:23 -0600

    vamos x1601

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
@@ -92,7 +92,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         cartQuantity += item.quantity;
       });
 
-      console.log(cartQuantity);
-      console.log(cart);
+      document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
   });
 });
