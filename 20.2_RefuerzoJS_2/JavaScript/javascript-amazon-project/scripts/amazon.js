24bc11d1a14849699a3a9fdd3389bba50e7e252f
adelso99 <adelsohernandez99@gmail.com>
2026-03-04 21:58:51 -0600

    vamos x1597

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/amazon.js
@@ -50,7 +50,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -65,12 +65,12 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 //añadiendo evento al boton de agregar para hacerlo interactivo
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () =>{ 
-      const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
       cart.forEach((item) => {
-        if(productName === item.productName){
+        if(productId === item.productId){
             matchingItem = item;
         }
       });
@@ -79,7 +79,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         matchingItem.quantity += +1;
       }else{
         cart.push({
-         productName: productName,
+         productId: productId,
          quantity: 1
         });
       }
