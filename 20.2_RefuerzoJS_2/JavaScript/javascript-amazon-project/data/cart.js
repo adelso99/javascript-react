605f25930c9238b379f2baca114590048e7f0163
adelso99 <adelsohernandez99@gmail.com>
2026-03-09 22:02:12 -0600

    vamos x1631

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/cart.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/cart.js
@@ -1,7 +1,10 @@
-export const cart = [
-
-  
-];
+export const cart = [{
+  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity: 2  
+}, {
+  productId: "4863415b6fc6f-327a-4ec4-896f-486349e85a3d9e85a3d",
+  quantity: 1  
+}];
 
 
 //Funcion para agregar al carrito
