4b27b71ad3f0c48e9e005bcf1e791297367199dd
adelso99 <adelsohernandez99@gmail.com>
2026-03-10 23:09:01 -0600

    vamos x1645

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -100,6 +100,5 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     link.addEventListener("click", () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
     });
 });
