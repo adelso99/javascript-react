bb535fbba9b03a66748a57629e62e2e7ba04eb2e
adelso99 <adelsohernandez99@gmail.com>
2026-03-13 23:56:40 -0600

    vamos x1668

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
             Update
           </span>
-            <input class="quantity-input">
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
             <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
           <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
@@ -154,14 +154,13 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.classList.remove("is-editing-quantity");
-
-    const dato = document.querySelector(".quantity-input");
-    const valorProducto = Number(dato.value);
-    console.log(valorProducto);
+    
+    //Parte de guardar la cantidad que se ingresa
+    const quantityInput  = document.querySelector(`.js-quantity-input-${productId}`);
+    const newQuantity  = Number(quantityInput.value);
+    console.log(newQuantity );
 
   });
-
-
   
 });
 
