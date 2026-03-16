f0fcb2f067ae3ad69a13dd19e47f9efc3e1b9195
adelso99 <adelsohernandez99@gmail.com>
2026-03-15 23:20:35 -0600

    vamos x1679

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -151,22 +151,31 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
   link.addEventListener("click", () => {
 
     const productId = link.dataset.productId;
-
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-
-    container.classList.remove("is-editing-quantity");
-    
+  
     //Parte de guardar la cantidad que se ingresa
     const quantityInput  = document.querySelector(`.js-quantity-input-${productId}`);
     const newQuantity  = Number(quantityInput.value);
 
+    //retorno anticipado
+    if (newQuantity < 0 || newQuantity >= 1000) {
+      alert('Quantity must be at least 0 and less than 1000');
+      return;
+    }
+
     //uso de la funcion creada
     updateQuantity(productId, newQuantity);
 
+    //se movio para aca el codigo
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.remove('is-editing-quantity');
+
+
     //Actulizacion de cantidad del carrito en el HTML
     const quantityLabel  = document.querySelector(`.js-quantity-label-${productId}`);
-    quantityLabel .innerHTML = newQuantity;
 
+    quantityLabel.innerHTML = newQuantity;
+
+    updateCartQuantity();
   
   });
   
