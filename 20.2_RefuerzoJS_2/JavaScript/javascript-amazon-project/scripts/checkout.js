d8fb291d7cd05c37a5331fd7f131855926c3c5de
adelso99 <adelsohernandez99@gmail.com>
2026-03-16 22:55:32 -0600

    vamos x1687

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -2,6 +2,8 @@ import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from "../da
 import {products} from "../data/products.js";
 import {formatCurrency} from "./utils/money.js";
 
+hello();
+
 let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
