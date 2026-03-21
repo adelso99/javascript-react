c447b1a24e39f211ca02474e142a7d01d594b3dd
adelso99 <adelsohernandez99@gmail.com>
2026-03-21 00:16:28 -0600

    vamos x1722

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -64,45 +64,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-options-title">
           Choose a delivery option:
         </div>
-        <div class="delivery-option">
-          <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-              FREE Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-              $4.99 - Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Monday, June 13
-            </div>
-            <div class="delivery-option-price">
-              $9.99 - Shipping
-            </div>
-          </div>
-        </div>
+        ${deliveryOptionsHTML(matchingProduct)}
       </div>
     </div>
   </div>
@@ -111,9 +73,9 @@ cart.forEach((cartItem) => {
 });
 
 //Creacion de la opcion de pago gratis, 7 dias o 1 dia
-function deliveryOptionsHTML(){
+function deliveryOptionsHTML(matchingProduct){
   let html = "";
-  
+
       deliveryOptions.forEach((deliveryOption) => {
         const today = dayjs();
         const deliveryDate = today.add(
@@ -130,7 +92,7 @@ function deliveryOptionsHTML(){
           ? "FREE"
           : `${formatCurrency(deliveryOption.priceCents)} -`;
 
-        `
+        html += `
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
@@ -146,6 +108,8 @@ function deliveryOptionsHTML(){
         </div>
         `
       });
+
+   return html;
 
 }
 
