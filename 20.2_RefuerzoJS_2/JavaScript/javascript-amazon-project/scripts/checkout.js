8a97bf06da2bc808c483fb8d0c42397a02bd7fa5
adelso99 <adelsohernandez99@gmail.com>
2026-03-21 21:41:16 -0600

    vamos x1727

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from "../data/cart.js";
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from "../data/cart.js";
 import {products} from "../data/products.js";
 import {formatCurrency} from "./utils/money.js";
 
@@ -117,7 +117,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
           const isChecked = deliveryOption.id === cartItem.deliveryOptionId; 
 
         html += `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
           <input type="radio"
           ${isChecked ? "checked" : ""}
             class="delivery-option-input"
@@ -224,4 +226,12 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
 });
 
 
+
+//Oyente para la funcion de elegir que opcion de entrega de producto
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  });
+});
 
