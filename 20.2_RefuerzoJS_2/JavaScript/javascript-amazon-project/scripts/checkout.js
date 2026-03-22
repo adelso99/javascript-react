108a1e0d6aed8639788fdddc4bb9ec191c68b2cd
adelso99 <adelsohernandez99@gmail.com>
2026-03-21 23:20:05 -0600

    vamos x1732

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
+import {renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 //Llamado de la funcion para que se ejecuta al realizar la carga de pagina
 renderOrderSummary();
+
+renderPaymentSummary();
