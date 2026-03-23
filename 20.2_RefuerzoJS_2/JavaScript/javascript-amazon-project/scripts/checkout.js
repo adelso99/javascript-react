802248e25c74606a4c421ed4c7b3f84a4d529c03
adelso99 <adelsohernandez99@gmail.com>
2026-03-22 22:52:17 -0600

    vamos x1739

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,15 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+//Importacion de daysj
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+
 //Llamado de la funcion para que se ejecuta al realizar la carga de pagina
 renderOrderSummary();
 
 renderPaymentSummary();
+
+const dato = dayjs();
+const dato2 = dato.add(5, "days");
+const fecha = dato2.format("MMMM D")
+console.log(fecha);
