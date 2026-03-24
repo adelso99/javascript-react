a50eb073728efe4ab5d6e678089884c3309c8481
adelso99 <adelsohernandez99@gmail.com>
2026-03-24 11:46:12 -0600

    vamos x1749

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -1,36 +1,10 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary } from "./checkout/paymentSummary.js";
 
-//Importacion de daysj
-import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 
 //Llamado de la funcion para que se ejecuta al realizar la carga de pagina
 renderOrderSummary();
 
 renderPaymentSummary();
 
-//importacion de la funcion isWeekend
-import isSatSun  from "./utils/money.js";
-
-const dato = dayjs();
-
-//Fecha 1
-let dato2 = dato.add(2, "days");
-let fecha = dato2.format('dddd, MMMM D');
-console.log(fecha);
-console.log(isSatSun (dato2));
-
-
-//Fecha 2
-dato2 = dato.add(4, "days");
-fecha = dato2.format('dddd, MMMM D');
-console.log(fecha);
-console.log(isSatSun (dato2));
-
-
-//Fecha 3
-dato2 = dato.add(5, "days");
-fecha = dato2.format('dddd, MMMM D');
-console.log(fecha);
-console.log(isSatSun (dato2));
 
--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/utils/money.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/utils/money.js
@@ -2,12 +2,5 @@ export function formatCurrency(priceCents){
    return (Math.round(priceCents) / 100).toFixed(2);
 }
 
-//export default formatCurrency;
+export default formatCurrency;
 
-
-function isWeekend(dato){
-  const fecha = dato.format("dddd");
-  return fecha === "Saturday" || fecha === "Sunday"
-};
-
-export default isWeekend; 
