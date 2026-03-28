0944c9738fb4d0c3536d20f2704cb4dab28c9f29
adelso99 <adelsohernandez99@gmail.com>
2026-03-27 21:52:57 -0600

    vamos x1774

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tests/moneyTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tests/moneyTest.js
@@ -1,11 +1,14 @@
 import {formatCurrency} from "../scripts/utils/money.js"
 
+console.log("Conversion de Centavos a Dolares");
+
 if(formatCurrency(2095) === "20.95"){
   console.log("Passed");
 }else{
   console.log("Failed");
 }
 
+console.log("Funciona con 0");
 
 if(formatCurrency(0) === "0.00"){
   console.log("Passed");
