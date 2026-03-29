8a8fdb269d4e61b83ebfdf795dcfa7899ede5437
adelso99 <adelsohernandez99@gmail.com>
2026-03-28 22:57:43 -0600

    vamos x1784

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/moneyTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/moneyTest.js
@@ -3,6 +3,6 @@ import {formatCurrency} from "../scripts/utils/money.js"
 
 describe("Conjunto de pruebas: formatCurrency --Formato de Moneda--", () => {
     it("Conversion de Centavos a Dolares", () => {
-      
+    expect(formatCurrency(2095) === "20.95");
     });
 });
