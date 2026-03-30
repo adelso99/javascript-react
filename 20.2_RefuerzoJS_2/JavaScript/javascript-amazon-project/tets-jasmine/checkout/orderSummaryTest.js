31b5d41943f5f05630ee253ca9f92cacc35fb27c
adelso99 <adelsohernandez99@gmail.com>
2026-03-29 18:47:05 -0600

    vamos x1791

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/checkout/orderSummaryTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/checkout/orderSummaryTest.js
@@ -1,1 +1,22 @@
 import {renderOrderSummary} from "../../scripts/checkout/orderSummary";
+import {loadFromStorage} from "../../data/cart.js";
+
+
+describe("Conjunto de Pruebas: renderOrderSummary --Resumen del Pedido--", () => {
+  it("muestra el carrito", () => {
+    document.querySelector(".js-test-container").innerHTML = `
+       <div class="js-order-summary"></div>
+    `;
+
+  spyOn(localStorage, "getItem").and.callFake(() => {
+    return JSON.stringify([{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionId: "1"
+    }]); //devuelviendo cadena en el localst  
+  }); 
+
+  loadFromStorage();
+
+  });
+});
--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/data/cartTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets-jasmine/data/cartTest.js
@@ -13,6 +13,7 @@ describe("Conjunto de Pruebas: addToCart --Añadir al Carrito--", () => {
     }); 
 
     loadFromStorage();
+    
       addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
       expect(cart.length).toEqual(1);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
