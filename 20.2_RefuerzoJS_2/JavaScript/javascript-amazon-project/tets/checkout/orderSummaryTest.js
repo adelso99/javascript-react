0a98f8677541377750ffd2539f9f574ee3c8194d
adelso99 <adelsohernandez99@gmail.com>
2026-03-31 16:39:45 -0600

    vamos x1803

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets/checkout/orderSummaryTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets/checkout/orderSummaryTest.js
@@ -95,6 +95,26 @@ const name1 = "Black and Gray Athletic Cotton Socks - 6 Pairs";
       expect(cart.length).toEqual(1);
       expect(cart[0].productId).toEqual(productId2);
 
-
   });
+
+  it('Conjunto de Pruebas: Actualizar la opción de entrega', () => {
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+    expect(
+      document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked
+    ).toEqual(true);
+
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+
+    expect(
+      document.querySelector('.js-payment-summary-shipping').innerText
+    ).toEqual('$14.98');
+    expect(
+      document.querySelector('.js-payment-summary-total').innerText
+    ).toEqual('$63.50');
+  });
+  
 });
+
