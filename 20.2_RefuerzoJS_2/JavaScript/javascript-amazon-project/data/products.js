3642df8641723a5cd23ccb03a78673e510fdd78c
adelso99 <adelsohernandez99@gmail.com>
2026-04-02 23:57:48 -0600

    vamos x1825

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/products.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
 //funcion de verificacion de Producto por el Id
 export function getProduct(productId){
   let matchingProduct;
@@ -27,32 +29,23 @@ class Product {
     this.priceCents = productDetails.priceCents;
   }
 
+
+  //metodos para que calculen las estrellas y el precio
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return ` $${formatCurrency(this.priceCents)}`;
+  }
+
 }
 
-const product1 = new Product(
-    {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: [
-        "socks",
-        "sports",
-        "apparel"
-      ]
-    }
-);
-
-console.log(product1);
-
+//Crearemos OPP en Herencia
 
 
 export const products = [
-  {
+ {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -710,4 +703,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  //conversion de cada producto del array en una nueva clase
+  return new Product(productDetails);
+});
+
