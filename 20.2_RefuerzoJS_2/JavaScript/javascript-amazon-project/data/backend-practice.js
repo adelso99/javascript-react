aefdb3fa938cc3b04170cb0ecae92e2a044247f4
adelso99 <adelsohernandez99@gmail.com>
2026-04-05 22:17:07 -0600

    vamos x1855

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/backend-practice.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/data/backend-practice.js
@@ -1,3 +1,11 @@
 //Practica para comunicacion entre front-ent y back-end con HTTP
-//uso de la clase "XMLHttpRequest"
-new XMLHttpRequest();
+
+//creacion de msj http con el uso de la clase "XMLHttpRequest"
+const xhr = new XMLHttpRequest();
+
+//confuracion de msj
+//se debe de configurar 2 parametros: P1. define el tipo de menaje http se envia y el P2. indica a la compu donde enviar ese mensaje http
+xhr.open("get", "https://supersimplebackend.dev"); //get: info del servidor -- URL es la comunicacion con el ordenador que se desea
+
+//enviar mensaje
+xhr.send();
