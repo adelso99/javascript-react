62cc391dc575934e8bed8c4ea042ff3d676b5c08
adelso99 <adelsohernandez99@gmail.com>
2026-04-07 23:58:20 -0600

    vamos x1871

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets/checkout/orderSummaryTest.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/tets/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from "../../scripts/checkout/orderSummary.js";
 import {loadFromStorage, cart} from "../../data/cart.js";
+import {loadProducts} from "../../data/products.js";
 
 
 describe("Conjunto de Pruebas: renderOrderSummary --Resumen del Pedido--", () => {
@@ -7,6 +8,13 @@ describe("Conjunto de Pruebas: renderOrderSummary --Resumen del Pedido--", () =>
 const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
 const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 const name1 = "Black and Gray Athletic Cotton Socks - 6 Pairs";
+
+  //uso de gancho para as pruebas ya que nuestros productos ahora vienen de back-end
+  beforeAll((done) => { //jasmin tiene una funcion done() que espera que el codigo cargue para ejecutar la funcion
+    loadProducts(() => {
+      done();
+    });
+  });
 
   //uso de Hooks o Ganchos con beforeEach()
   beforeEach(() => {
