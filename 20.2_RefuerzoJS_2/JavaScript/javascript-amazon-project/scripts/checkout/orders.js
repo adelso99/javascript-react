b0bcee3e3ef8dc43d09cb43fc46cf0c77d2d7ec8
adelso99 <adelsohernandez99@gmail.com>
2026-04-20 23:59:31 -0600

    vamos x1961

--- 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout/orders.js
+++ 20.2_RefuerzoJS_2/JavaScript/javascript-amazon-project/scripts/checkout/orders.js
@@ -1,7 +1,7 @@
-import {getProduct, loadProductsFetch} from '../data/products.js';
-import {orders} from '../data/orders.js';
+import {getProduct, loadProductsFetch } from '../../data/products.js';
+import { orders } from '../../data/orders.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import formatCurrency from './utils/money.js';
+import formatCurrency from '../utils/money.js';
 
 async function loadPage() {
   await loadProductsFetch();
