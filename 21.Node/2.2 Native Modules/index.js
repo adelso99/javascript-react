13b18af9785f14d1d3e7dcc3a4c9091d2e54d594
adelso99 <adelsohernandez99@gmail.com>
2026-04-23 21:39:39 -0600

    vamos x1980

--- 21.Node/2.2 Native Modules/index.js
+++ 21.Node/2.2 Native Modules/index.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hola con Node,js", (err) => {
+fs.writeFile("message.txt", "Hola con Node.js", (err) => {
   if (err) throw err;
   console.log('El Archivo se ha Guadado!');
+
+  fs.readFile('message.txt', "utf8",(err, data) => {
+    if (err) throw err;
+      // 1. crear nuevo contenido
+      let data = "Hola con un Cambio";
+  
+      // 2. guardar el cambio
+      fs.writeFile('message.txt', nuevo, (err) => {
+        if (err) throw err;
+        console.log("Cambio exitoso");
+      });
+  }); 
+  
 }); 
+
