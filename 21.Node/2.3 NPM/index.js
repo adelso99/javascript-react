1a9ee59a7573df22b95a056de091c385cdc646ef
adelso99 <adelsohernandez99@gmail.com>
2026-04-25 23:15:25 -0600

    vamos x1992

--- 21.Node/2.3 NPM/index.js
+++ 21.Node/2.3 NPM/index.js
@@ -1,4 +1,11 @@
-var generateName = require('sillyname');
+//var generateName = require('sillyname');
+
+import generateName from "sillyname";
+import superheroes from 'superheroes';
+
+
 var sillyName = generateName();
+var superHeroes = superheroes.name;
 
 console.log(`Mi nombre es ${sillyName}.`);
+console.log(`Mi nombre de Super Heroe es ${superHeroes}.`);
