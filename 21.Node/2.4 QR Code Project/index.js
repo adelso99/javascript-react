5236cee6fd79eb6ac5a20401886613d290bdd88e
adelso99 <adelsohernandez99@gmail.com>
2026-04-28 23:42:56 -0600

    vamos x2015

--- 21.Node/2.4 QR Code Project/index.js
+++ 21.Node/2.4 QR Code Project/index.js
@@ -14,32 +14,32 @@ import fs from "fs";
 
 //1. Use the inquirer npm package to get user input.
 
-inquirer
-  .prompt([
-    {
-      "message": "Cual es tu Tipo de URL: ", 
-      "name": "URL"
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+  inquirer
+    .prompt([
+      {
+        "message": "Cual es tu Tipo de URL: ", 
+        "name": "URL"
+      },
+    ])
+    .then((answers) => {
+      const url = answers.URL;
+      var qr_svg = qr.image(url);
+      qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-
-
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
 
 
 
-  
+
+
+
 
   /*
 process.stdin.on("data", (input) => {
