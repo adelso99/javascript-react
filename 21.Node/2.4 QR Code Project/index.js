acbbd6fe45a87ec2357c6b98a62056e5aaef07c2
adelso99 <adelsohernandez99@gmail.com>
2026-04-29 23:32:35 -0600

    vamos x2018

--- 21.Node/2.4 QR Code Project/index.js
+++ 21.Node/2.4 QR Code Project/index.js
@@ -26,7 +26,7 @@ import fs from "fs";
       var qr_svg = qr.image(url);
       qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-      fs.writeFile('URL.txt', data, (err) => {
+      fs.writeFile('URL.txt', url, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
       }); 
