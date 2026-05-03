1c80d6510cb3e0e2e79af2cfa2b7012e32bafe01
adelso99 <adelsohernandez99@gmail.com>
2026-05-02 22:17:09 -0600

    vamos x2039

--- 21.Node/3.2 HTTP Requests/index1.js
+++ 21.Node/3.2 HTTP Requests/index1.js
@@ -5,7 +5,10 @@ const port = 3000;
 
 //peticion GET
 app.get("/", (req, res) => {
-    console.log(req.rawHeaders);
+    //console.log(req.rawHeaders);
+
+    //envio de msj
+    res.send("Hola desde Express");
 });
 
 app.listen(port, () => {
