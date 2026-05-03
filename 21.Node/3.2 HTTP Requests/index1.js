73022e71f73968ed5e8e02b0b862e3bae08e9436
adelso99 <adelsohernandez99@gmail.com>
2026-05-02 22:14:02 -0600

    vamos x2038

--- 21.Node/3.2 HTTP Requests/index1.js
+++ 21.Node/3.2 HTTP Requests/index1.js
@@ -2,6 +2,12 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+
+//peticion GET
+app.get("/", (req, res) => {
+    console.log(req.rawHeaders);
+});
+
 app.listen(port, () => {
   console.log(`Servidor corrigiendo en el puerto ${port}`);
 });
