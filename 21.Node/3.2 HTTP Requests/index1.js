d76036a9b25f8c7cc8c8d0f516948baec7609187
adelso99 <adelsohernandez99@gmail.com>
2026-05-03 22:25:45 -0600

    vamos x2046

--- 21.Node/3.2 HTTP Requests/index1.js
+++ 21.Node/3.2 HTTP Requests/index1.js
@@ -18,10 +18,13 @@ app.get("/about", (req, res) => {
     //console.log(req.rawHeaders);
 
     //envio de msj
-    res.send("<h1>Uso de Endpoint</h1>");
+    res.send("<h1>Hola desde Sobre Mi</h1>");
 });
 
 
+app.get("/contact", (req, res) => {
+  res.send("<h1>Hola desde Contactame</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Servidor corrigiendo en el puerto ${port}`);
