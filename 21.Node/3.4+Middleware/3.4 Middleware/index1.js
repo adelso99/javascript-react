bc3c9b632dfca6288ea74ed43ab6f8eecccb06ad
adelso99 <adelsohernandez99@gmail.com>
2026-05-16 23:44:27 -0600

    vamos x2131

--- 21.Node/3.4+Middleware/3.4 Middleware/index1.js
+++ 21.Node/3.4+Middleware/3.4 Middleware/index1.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-//uso del middleware
+//uso del middleware con importacion de bodyParser
 const app = express();
 app.use(
    bodyParser.urlencoded({
