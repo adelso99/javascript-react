b69407a46b59f92de028eddfca5950098b23c3f5
adelso99 <adelsohernandez99@gmail.com>
2026-07-11 23:21:50 -0600

    vamos x2501

--- 22.EJS/4.0.1/index.js
+++ 22.EJS/4.0.1/index.js
@@ -1,6 +1,7 @@
 //Importacion de paquetes para uso de servidor
 import express from "express";
 
+
 //variabale para uso de express
 const app = express();
 
@@ -10,5 +11,6 @@ let fecha = new Date();
 
 //verificacion en consola
 console.log(fecha.getDay());
+
 
 //uso de if o switch
