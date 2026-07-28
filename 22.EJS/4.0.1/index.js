db23fb60e33f9543032b9cc79a9d1e7c605138fe
adelso99 <adelsohernandez99@gmail.com>
2026-07-28 10:24:29 -0600

    vamos x2613

--- 22.EJS/4.0.1/index.js
+++ 22.EJS/4.0.1/index.js
@@ -1,14 +1,11 @@
 //Importacion de paquetes para uso de servidor
 import express from "express";
 
-
 //variabale para uso de express
 const app = express();
 
-
 //vaiables de fecha
 let fecha = new Date();
-
 
 //verificacion en consola
 console.log(fecha.getDay());
