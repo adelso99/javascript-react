f93d1e7626c9d9a93f8c9b2fa76d3f7781dd600a
adelso99 <adelsohernandez99@gmail.com>
2026-07-30 11:33:27 -0600

    vamos x2623

--- 22.EJS/4.0.1/index.js
+++ 22.EJS/4.0.1/index.js
@@ -1,21 +1,23 @@
-//Importacion de paquetes para uso de servidor
-import express from "express";
+//Importacion de paquetes para uso de servidor 
+import express from "express"; 
 
-//variabale para uso de express
-const app = express();
+//variabale para uso de express 
+const app = express(); 
 
-//vaiables de fecha
-let fecha = new Date();
+//vaiables de fecha 
+let fecha = new Date(); 
 
-//verificacion en consola
-console.log(fecha.getDay());
+//verificacion en consola 
+let dia = fecha.getDay(); 
+console.log(dia); 
 
-let dia = fecha.getDay();
-
-console.log(dia);
-
-//uso de if o switchz en este ejericicio
-
+//uso de if o switchz en este ejericicio 
+if(dia = 1){ console.log("Es día laboral, mantente enfocado"); 
+  }else if( dia === 6){ 
+    console.log("¡Disfruta tu fin de semana!"); 
+  }else if(dia === 0){
+     console.log("Descansa y prepárate para la semana."); 
+    }
 
 
 
