6f7cac06829210bba1af5677bc70847d25d77702
adelso99 <adelsohernandez99@gmail.com>
2026-08-18 14:15:33 -0600

    vamos x2750

--- 22.EJS/4.0.1/index.js
+++ 22.EJS/4.0.1/index.js
@@ -33,5 +33,4 @@ if(dia = 1){
     console.log("Es día laboral, mantente enfocado"); 
     console.log("Es día laboral, mantente enfocado"); 
     console.log("Es día laboral, mantente enfocado"); 
-    console.log("Es día laboral, mantente enfocado"); 
 */
