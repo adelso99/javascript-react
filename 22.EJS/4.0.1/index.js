134e277a3bcb96688778675ccc5950f2c7ab4136
adelso99 <adelsohernandez99@gmail.com>
2026-08-29 02:08:39 -0600

    vamos x2811

--- 22.EJS/4.0.1/index.js
+++ 22.EJS/4.0.1/index.js
@@ -34,4 +34,5 @@ if(dia = 1){
     console.log("Es día laboral, mantente enfocado"); 
     console.log("Es día laboral, mantente enfocado"); 
     console.log("Es día laboral, mantente enfocado"); 
+    console.log("Es día laboral, mantente enfocado"); 
 */
